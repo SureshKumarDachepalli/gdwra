93ec8e33dd6a6c0d09152fff97c4ed5e77af8523
dell <dell@DESKTOP-I89L11M>
2024-02-08 22:07:15 +0530

    i am adding two class in commit message

---
+++ src/main/java/pkg1/A.java
@@ -0,0 +1,10 @@
+package pkg1;
+
+public class A {
+
+	public static void main(String[] args) {
+		System.out.println(" i am push the code src/main");
+
+	}
+
+}
---
+++ src/test/java/pkg2/B.java
@@ -0,0 +1,10 @@
+package pkg2;
+
+public class B {
+
+	public static void main(String[] args) {
+		System.out.println(" i am push the code src/test");
+
+	}
+
+}
